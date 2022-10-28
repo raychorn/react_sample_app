a43f6a66ae5f4b3c67e6d01885d29457d734b33a
Ray C Horn <raychorn@gmail.com>
2022-10-28 12:59:13 +0000

    latest.

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -12,7 +12,25 @@ const uid = 'f3962f6d-ffe3-440f-bb1c-eef002ab5e66';
 
 const getUrl_uri = 'http://127.0.0.1:8088/6f7e6060-1e45-4fd1-9ff5-b5d6da9b8c64/singledomain/geturl/';
 
-//var createReactClass = require('create-react-class');
+var createReactClass = require('create-react-class');
+
+var Iframe = createReactClass({
+  render: function() {
+    console.log(this.props.visible);
+    if (this.props.visible === '1') {
+      return(         
+        <div>          
+          <iframe src={this.props.src} title={this.props.title} height={this.props.height} width={this.props.width}/>         
+        </div>
+      )
+    } else {
+      return(         
+        <div>          
+        </div>
+      )
+    }
+  }
+});
 
 function App() {
 
@@ -32,8 +50,13 @@ function App() {
 
 
   useEffect(() => {
-    fetchData()
-  }, []);
+    /*
+    if (theurl.response === undefined) {
+      fetchData();
+    }
+    */
+    fetchData();
+  }, [userid]);
 
   const handleLoginClick = (e) => {
     e.preventDefault();
@@ -51,7 +74,7 @@ function App() {
   let html_goes_here;
   if (userid === uid) {
     button_goes_here = <button onClick={handleLogoutClick}>Logout</button>;
-    html_goes_here = <div dangerouslySetInnerHTML={{ __html: "<iframe width=800 height=400 src='https://securex.ai' />"}} />;
+    html_goes_here = <div>Show the IFrame now.</div>;
   } else {
     button_goes_here = <button onClick={handleLoginClick}>Login</button>;
     html_goes_here = <div></div>;
@@ -59,21 +82,22 @@ function App() {
 
   //if (error) return <p>An error occurred</p>
 
-  // theurl.response['singledomain.geturl'].url
+  // {theurl.response && theurl.response['singledomain.geturl'].url}
 
   return (
     <div className="App">
       <header className="App-header">
         <small><b>theurl:</b>&nbsp;
-        {
-            theurl.response && theurl.response['singledomain.geturl'].url
-        }        
+        {theurl.response && theurl.response['singledomain.geturl'].url}
         </small>
+        <br />
+        {userid}, {userid === uid ? 'logged in' : 'not logged in'}
         <br />
         {button_goes_here}
         <br />
         {html_goes_here}
         </header>
+        <Iframe src={theurl.response && theurl.response['singledomain.geturl'].url} visible="{userid === uid ? '1' : '0'}" title="content" height="400" width="800"/>
     </div>
   );
 }
